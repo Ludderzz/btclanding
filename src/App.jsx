04fb29944539aa7bfdc8025f827118319053acae
Eddie Ludlam <eludlam1221@gmail.com>
2026-02-06 00:23:26 +0000

    appjsxupdate2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
-import Unsubscribe from './pages/Unsubscribe';
+import Unsubscribe from './pages/Unsubscribe'; // Make sure this file exists in src/pages/
 
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/unsubscribe" element={<Unsubscribe />} />
+        {/* Fallback route: if they go to a broken link, send them home */}
+        <Route path="*" element={<Landing />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;
